9e7d68fe90d2fb85e02cc15a28efe071d4a22191
xavizalote <xavier.cazalot@gmail.com>
2016-03-03 17:33:53 +0100

    img prop for Profilepic component

--- client/components/Profilepic.jsx
+++ client/components/Profilepic.jsx
@@ -4,7 +4,7 @@ export default class Profilepic extends React.Component {
   render () {
     return (
       <div style={{width: this.props.size, height: this.props.size, marginLeft: 'auto', marginRight: 'auto', marginBottom: '15px'}}>
-        <img src={this.props.picture} className="profile-pic" />
+        <img src={this.props.img} className="profile-pic" />
       </div>
     );
   }
@@ -12,5 +12,5 @@ export default class Profilepic extends React.Component {
 
 Profilepic.propTypes = {
   size: React.PropTypes.string.isRequired,
-  picture: React.PropTypes.string.isRequired
+  img: React.PropTypes.string.isRequired
 };
---
+++ client/lib/data.js
@@ -0,0 +1,3 @@
+/**
+ * Created by xavier on 03/03/2016.
+ */
