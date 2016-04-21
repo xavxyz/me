82376210110f109927d5aa0fbb5cef3f89bc0bf8
xavizalote <xavier.cazalot@gmail.com>
2016-04-21 14:47:06 +0100

    update to react from nom

--- client/lib/data.js
+++ client/lib/data.js
@@ -4,31 +4,32 @@ export default Data = {
 		img: 'https://pbs.twimg.com/profile_images/704641906370879488/9crxlIge_400x400.jpg'
 	},
 	headers: [
-		{ _id: 1, text: 'Xavier Cazalot <span class="purple_light">0</span><span class="purple">.</span><span class="purple_light">3</span>' },
+		{ _id: 1, text: 'Xavier Cazalot <span class="purple_light">0</span><span class="purple">.</span><span class="purple_light">4</span>' },
 		{ _id: 2, text: 'Hacker. Entrepreneur. Backpacker. ' },
-		{ _id: 3, text: 'Stalk me on <a href="https://twitter.com/xav_cz" target="_blank">Twitter</a>' }
+		{ _id: 3, text: 'Stalk me on <a href="https://twitter.com/xav_cz" target="_blank">Twitter</a>' },
+		{ _id: 4, text: 'Read me on <a href="http://hacklearnmake.com/" target="_blank">hacklearnmake.com</a>' }
 	],
 	blocks: [
 		{
 			_id: 1,
-			company: 'Freelance',
-			job: 'Dev MeteorJS',
+			company: 'Consultant',
+			job: 'Software Engineer MeteorJS',
 			slogan: 'Value Proposition Building',
-			description: "I am a [data-driven](http://leananalyticsbook.com/) entrepreneur and a freelance developer working with [MeteorJS](https://www.meteor.com), a client-server-database framework which allows to concentrate on developing business value. I help entrepreneurs making [remarkable products](https://en.wikipedia.org/wiki/Purple_Cow:_Transform_Your_Business_by_Being_Remarkable) for targeted customers. "
+			description: "I am a [data-driven](http://leananalyticsbook.com/) entrepreneur and a consultant software engineer working with [MeteorJS](https://www.meteor.com), a client-server-database framework which allows to concentrate on developing business value. I help entrepreneurs making [remarkable products](https://en.wikipedia.org/wiki/Purple_Cow:_Transform_Your_Business_by_Being_Remarkable) for targeted customers."
 		},
 		{
 			_id: 2,
 			company: 'Startup42',
 			job: 'Program Manager',
 			slogan: 'From Hackers To Founders',
-			description: "[Startup42](http://www.startup42.org) is a non-profit accelerator designed by [EPITA](http://www.epita.fr/international/) to offer hacker teams, having a good idea, the skills and opportunities they need to execute like real startup founders. I have been running the daily operational jobs of the program during the 6th session with [Maxime Pico](https://linkedin.com/in/maximepico). "
+			description: "[Startup42](http://www.startup42.org) is a non-profit accelerator designed by [EPITA](http://www.epita.fr/international/) to offer hacker teams, having a good idea, the skills and opportunities they need to execute like real startup founders. I have been running the daily operational jobs of the program during the 6th session with [Maxime Pico](https://linkedin.com/in/maximepico)."
 		},
 		{
 			_id: 3,
 			company: 'Me',
 			job: 'Good fellow',
-			slogan: 'Hack, Learn, Make',
-			description: "Established in Paris, France, if not traveling [around the world](https://vimeo.com/sakados). Originally from the south of France. I'm also partner in a [skateboard company](http://www.le-shape.com). I love everything related to Hacking, [Slack](https://slack.com), Skating and Traveling. Contact me if I can help you!"
+			slogan: '{<a href="http://hacklearnmake.com" target="_blank">Hack</a>, <a href="http://hacklearnmake.com" target="_blank">Learn</a>} = <a href="http://hacklearnmake.com" target="_blank">Make</a>',
+			description: "Living in Paris, France, if not traveling [around the world](https://vimeo.com/sakados). Originally from the south of France. I love everything related to Hacking, [Slack](https://slack.com), Skateboarding and Traveling. I write about startup execution and useful hacks with Meteor on [hacklearnmake.com](http://hacklearnmake.com)."
 		}
 	],
 	links: [
