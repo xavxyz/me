ddd083b70454cf8d12e6f5e63831dfe071b5861d
xavcz <xavier.cazalot@gmail.com>
2016-08-10 17:12:12 +0200

    no s(h)erif ⭐️🔫, deploy script 🕹

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+git commit -am "🏠"
+git checkout -B gh-pages
+git add -f build
+git commit -am "🛠"
+git filter-branch -f --prune-empty --subdirectory-filter build
+git push -f origin gh-pages
+git checkout -
