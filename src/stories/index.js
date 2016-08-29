e078329a753f17cfb8d6b8cd4a23e9a4bbad25e7
xavcz <xavier.cazalot@gmail.com>
2016-08-29 12:00:23 +0200

    fix button effect

--- src/stories/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import App from '../App';
-
-storiesOf('App', module)
-  .add('Default View', () => (
-    <App />
-  ));
