66df09e69a4f00daa67dbef4345236240119a576
xavcz <xavier.cazalot@gmail.com>
2016-09-01 17:39:16 +0200

    static page 🐔

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-git commit -am "deploy 🚀"
-git checkout -B gh-pages
-git add -f build
-git commit -am "🛠"
-git filter-branch -f --prune-empty --subdirectory-filter build
-git push -f origin gh-pages
-git checkout -
