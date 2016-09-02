7cc5ebebb885fd60a0bc62e8fe17a8874f266463
xavcz <xavier.cazalot@gmail.com>
2016-09-02 10:59:58 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 const app = express();
 
-app.set('view engine', 'jade');
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://xavcz.github.io/cazalot-bros/', 'http://localhost:8000', 'http://xav.cz'];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res, next) => {
   res.send('Hello \\o/');
 });
+
 app.get('/email', (req, res, next) => {
   if (req.query.email && req.query.firstname) {
     sendgrid.send({
