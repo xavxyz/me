5dae32a4762c28bc417777e49108cb737d10f769
xavcz <xavier.cazalot@gmail.com>
2016-09-12 16:44:50 +0200

    🏠

--- elm/deploy.sh
+++
@@ -1,7 +0,0 @@
-git commit -am "🏠"
-git checkout -B gh-pages
-git add -f dist
-git commit -am "⚗"
-git filter-branch -f --prune-empty --subdirectory-filter dist
-git push -f origin gh-pages
-git checkout -
