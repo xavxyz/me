ea2324b7bdbaa1522981100a59de4e1b0ce66c45
xavcz <xavier.cazalot@gmail.com>
2016-12-22 15:40:37 +0100

    don't loose CNAME when building

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cd ..
 git commit -am "build & deploy 🚀"
 git checkout -B gh-pages
 git add -f elm/dist
+echo 'xav.cz' > elm/dist/CNAME
 git commit -am "⚗"
 git filter-branch -f --prune-empty --subdirectory-filter elm/dist
 git push -f origin gh-pages
