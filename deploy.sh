bfc8d8777a7f5f6035899817aa884be52ca466df
xavcz <xavier.cazalot@gmail.com>
2016-12-22 15:42:13 +0100

    add CNAME before commit

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ elm-app build
 cd ..
 git commit -am "build & deploy 🚀"
 git checkout -B gh-pages
-git add -f elm/dist
 echo 'xav.cz' > elm/dist/CNAME
+git add -f elm/dist
 git commit -am "⚗"
 git filter-branch -f --prune-empty --subdirectory-filter elm/dist
 git push -f origin gh-pages
